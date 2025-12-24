ec6b3826900db8ead0bd1a09f5b41f85d683b5b6
Boris Ushakov <inlvert@gmail.com>
2025-12-24 15:51:54 +0200

    add dto

---
+++ server/src/products/dto/create-product.dto.ts
@@ -0,0 +1,21 @@
+import { IsString, IsNumber } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly description: string;
+
+  @IsString()
+  readonly price: string;
+
+  @IsString()
+  readonly category: string;
+
+  @IsString({ each: true })
+  readonly images: string[];
+
+  @IsNumber()
+  readonly stock: number;
+}
