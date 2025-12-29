13bddc2af3013245ba56980c3006e86337d220fd
Boris Ushakov <inlvert@gmail.com>
2025-12-29 23:55:23 +0200

    add static

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    index: false,
+  });
+
   await app.listen(process.env.PORT ?? 5000);
 }
+
 bootstrap();
