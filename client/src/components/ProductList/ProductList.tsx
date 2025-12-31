2f1f72f47b01fb92366b316df9d062605218df01
Boris Ushakov <inlvert@gmail.com>
2025-12-31 17:21:34 +0200

    add changes

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 import ProductItem from "../ProductItem/ProductItem";
 import styles from "./ProductList.module.scss";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
@@ -11,26 +11,22 @@ export default function ProductList() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getProducts())
-  }, []);
+    dispatch(getProducts());
+  }, [dispatch]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  const foundProducts = products.map((product) => (
-    <ProductItem key={product._id} product={product} />
-  ));
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className={`${styles.container} w-full max-w-[1440px] h-auto `}>
+    <section className={styles.container}>
       <h1 className={styles.title}>Our Products</h1>
-      {foundProducts}
-      {/* Product items would be rendered here */}
-    </div>
+
+      {/* 🔥 ОКРЕМИЙ GRID */}
+      <div className={styles.productsGrid}>
+        {products.map((product) => (
+          <ProductItem key={product._id} product={product} />
+        ))}
+      </div>
+    </section>
   );
 }
