52624596a6e1a9343ffa51ced3f63c2ffe97c989
Boris Ushakov <inlvert@gmail.com>
2026-01-03 14:31:50 +0200

    add changes

--- client/src/components/ProductItem/ProductItem.tsx
+++ client/src/components/ProductItem/ProductItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ButtonAddToCart from "../ButtonAddToCart/ButtonAddToCart";
 import styles from "./ProductItem.module.scss";
 import placeholderImg from "@/assets/placeholder.png";
 
@@ -11,10 +12,9 @@ type Product = {
 };
 
 export default function ProductItem({ product }: { product: Product }) {
-  const imageSrc =
-    product.images?.length
-      ? `http://localhost:5000/images/${encodeURIComponent(product.images[0])}`
-      : placeholderImg.src;
+  const imageSrc = product.images?.length
+    ? `http://localhost:5000/images/${encodeURIComponent(product.images[0])}`
+    : placeholderImg.src;
 
   return (
     <article className={styles.card}>
@@ -29,6 +29,9 @@ export default function ProductItem({ product }: { product: Product }) {
         <div className={styles.footer}>
           <span className={styles.price}>${product.price}</span>
         </div>
+      </div>
+      <div className={styles.addToCartWrapper}>
+        <ButtonAddToCart text="Add to cart" onClick={() => {}} />
       </div>
     </article>
   );
