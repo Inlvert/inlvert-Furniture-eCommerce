844fd1826015cad49ba6c0793135e554cf4379d7
Boris Ushakov <inlvert@gmail.com>
2026-01-06 15:37:13 +0200

    add images file fo sliders

---
+++ client/src/components/Slider/images.ts
@@ -0,0 +1,22 @@
+const images = [
+  {
+    id: 1,
+    title: "Inner Peace",
+    room: "Bed Room",
+    url: "/images/Rectangle24.png",
+  },
+  {
+    id: 2,
+    title: "Modern Comfort",
+    room: "Living Room",
+    url: "/images/Rectangle25.png",
+  },
+  {
+    id: 3,
+    title: "Warm Dining",
+    room: "Dining Room",
+    url: "/images/Rectangle26.png",
+  },
+];
+
+export default images;
