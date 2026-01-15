e06001e04cebdc76af82f37d9e5bae9f99d81ce6
Boris Ushakov <inlvert@gmail.com>
2026-01-15 14:29:44 +0200

    add photos for gallery

---
+++ client/src/components/Gallery/galleryImages.ts
@@ -0,0 +1,58 @@
+const images = [
+  {
+    id: 1,
+    src: "/images/Rectangle36.png",
+    className: "styles.img1",
+    column: "left",
+  },
+  {
+    id: 2,
+    src: "/images/Rectangle37.png",
+    className: "styles.img2",
+    column: "left",
+  },
+  {
+    id: 3,
+    src: "/images/Rectangle38.png",
+    className: "styles.img3",
+    column: "left",
+  },
+  {
+    id: 4,
+    src: "/images/Rectangle39.png",
+    className: "styles.img4",
+    column: "center",
+  },
+  {
+    id: 5,
+    src: "/images/Rectangle40.png",
+    className: "styles.img5",
+    column: "right",
+  },
+  {
+    id: 6,
+    src: "/images/Rectangle41.png",
+    className: "styles.img6",
+    column: "right",
+  },
+  {
+    id: 8,
+    src: "/images/Rectangle43.png",
+    className: "styles.img8",
+    column: "left",
+  },
+  {
+    id: 9,
+    src: "/images/Rectangle44.png",
+    className: "styles.img9",
+    column: "right",
+  },
+  {
+    id: 7,
+    src: "/images/Rectangle45.png",
+    className: "styles.img7",
+    column: "right",
+  },
+];
+
+export default images;
