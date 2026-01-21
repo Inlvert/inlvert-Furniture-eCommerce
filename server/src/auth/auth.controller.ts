7d4dd44a77c0c815d6d9a1862f5630219f2cb811
Boris Ushakov <inlvert@gmail.com>
2026-01-21 12:52:39 +0200

    add new rout to AuthController

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,16 +1,44 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-users.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from '@nestjs/passport';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly usersService: UsersService,
     private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
   ) {}
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {
+    // Guard redirects
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleCallback(@Req() req, @Res({ passthrough: true }) res: Response) {
+    const token = await this.authService.login(req.user);
+    return res.redirect(
+      `${process.env.FRONTEND_URL}/oauth?token=${token.accessToken}`,
+    );
+  }
 
   @Post('registration')
   async registration(@Body() createUserDto: CreateUserDto) {
@@ -28,6 +56,11 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
-    return { id: req.user.id, email: req.user.email, firstName: req.user.firstName, lastName: req.user.lastName};
+    return {
+      id: req.user.id,
+      email: req.user.email,
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+    };
   }
 }
