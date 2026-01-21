8cda4117010ca18e81c2630d423caa0fd4219e19
Boris Ushakov <inlvert@gmail.com>
2026-01-21 12:53:01 +0200

    add changes to schema

--- server/src/users/schema/users.schema.ts
+++ server/src/users/schema/users.schema.ts
@@ -5,17 +5,23 @@ export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true })
-  firstName: string;
-
-  @Prop({ required: true })
-  lastName: string;
-
   @Prop({ required: true, unique: true })
   email: string;
 
-  @Prop({ required: true })
-  password: string;
+  @Prop()
+  password?: string;
+
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  googleId?: string;
+
+  @Prop()
+  avatar?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
