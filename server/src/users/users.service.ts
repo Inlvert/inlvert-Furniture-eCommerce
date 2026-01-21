75581274adc12b92635e11ab8363e33b539da2b5
Boris Ushakov <inlvert@gmail.com>
2026-01-21 12:53:32 +0200

    add changes to UsersService

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,34 +1,40 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './schema/users.schema';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-users.dto';
-import bcrypt from 'bcryptjs';
+import { OAuthUserDto } from 'src/auth/dto/oauth-user.dto';
+import { User, UserDocument } from './schema/users.schema';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserDocument> {
-    const user = await this.userModel.findOne({ email: createUserDto.email });
-
-    if (user) throw new BadRequestException('this user was exist');
+    const exist = await this.userModel.findOne({ email: createUserDto.email });
+    if (exist) throw new BadRequestException('User already exists');
 
     const hashed = await bcrypt.hash(createUserDto.password, 10);
 
-    const newUser = await this.userModel.create({
-      ...createUserDto,
-      password: hashed,
-    });
-
-    newUser.save();
-
-    console.log('newUser', newUser);
-
-    return newUser;
+    return this.userModel.create({ ...createUserDto, password: hashed });
   }
 
   async findByEmail(email: string): Promise<UserDocument | null> {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  async findOrCreateByGoogle(oauthUser: OAuthUserDto): Promise<UserDocument> {
+    let user = await this.userModel.findOne({ email: oauthUser.email });
+
+    if (user) {
+      if (!user.googleId && oauthUser.googleId) {
+        user.googleId = oauthUser.googleId;
+        user.avatar = oauthUser.avatar;
+        await user.save();
+      }
+      return user;
+    }
+
+    return this.userModel.create({ ...oauthUser, password: undefined });
   }
 }
