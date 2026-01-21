fc115b3e13411dcc9ead1c18df25d93c346612dc
Boris Ushakov <inlvert@gmail.com>
2026-01-21 17:31:56 +0200

    add comp to Login page

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,9 +1,11 @@
+import Frame from "@/components/Frame/Frame";
 import Navbar from "@/components/Navbar/Navbar";
 
 function Login() {
   return (
-    <div className="flex justify-center">
+    <div className="flex items-center  flex-col">
       <Navbar />
+      <Frame title="Login" />
     </div>
   )
 }
