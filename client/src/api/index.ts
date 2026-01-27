8a1fc0b6d47517ddf1a819703525289a4335e6de
Boris Ushakov <inlvert@gmail.com>
2026-01-27 14:32:56 +0200

    add new api

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -6,7 +6,58 @@ const httpClient = axios.create({
   baseURL: "http://localhost:5000",
 });
 
+export interface LoginDto {
+  email: string;
+  password: string;
+}
+
+// Request interceptor
+httpClient.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+httpClient.interceptors.response.use(
+  function (response) {
+    if (response?.data?.accessToken) {
+      const token = response.data.accessToken;
+      localStorage.setItem("accessToken", token);
+    }
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 export const getProducts = async () => {
   const response = await httpClient.get("/products");
   return response.data;
+};
+
+// export const login = async (email: string, password: string) => {
+//   const response = await httpClient.post("/auth/login", {
+//     email,
+//     password,
+//   });
+//   return response.data;
+// }
+
+export const login = (userData: LoginDto) => {
+  const response = httpClient.post("/auth/login", userData);
+  return response
+}
+
+export const getProfile = async () => {
+  const response = await httpClient.get("/auth/profile");
+  return response;
 };
