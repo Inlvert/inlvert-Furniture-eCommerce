a788e106926e5a6883f9b45a12a6b1bd8414f50b
Boris Ushakov <inlvert@gmail.com>
2026-01-27 14:33:46 +0200

    add slice to store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,12 @@
 "use client";
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/productSlice";
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
-    products: productReducer
+    products: productReducer,
+    auth: authReducer,
   },
 });
 
