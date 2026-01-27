0572bdca6de7e966c5a6934b59980ec957ae44d6
Boris Ushakov <inlvert@gmail.com>
2026-01-27 14:34:35 +0200

    add login page

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,13 +1,36 @@
+"use client";
+
 import Frame from "@/components/Frame/Frame";
+import Greeting from "@/components/Greeting/Greeting";
+import LoginForm from "@/components/LoginForm/LoginForm";
 import Navbar from "@/components/Navbar/Navbar";
+import { useAppDispatch } from "@/redux/hooks";
+import { getProfile } from "@/redux/slices/authSlice";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 function Login() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    } else {
+      localStorage.setItem("accessToken", token);
+      dispatch(getProfile());
+    }
+  }, [token, dispatch]);
+
   return (
     <div className="flex items-center  flex-col">
       <Navbar />
       <Frame title="Login" />
+      <Greeting />
+      <LoginForm />
     </div>
-  )
+  );
 }
 
 export default Login;
