a7090270ba532bfe21d7fed7238ae485f10757b8
Boris Ushakov <inlvert@gmail.com>
2026-01-28 12:20:37 +0200

    add new page Signup

---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Frame from "@/components/Frame/Frame";
+import Greeting from "@/components/Greeting/Greeting";
+import Navbar from "@/components/Navbar/Navbar";
+import SignupForm from "@/components/SingupForm/SingupForm";
+import { useAppDispatch } from "@/redux/hooks";
+import { getProfile } from "@/redux/slices/authSlice";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+function Signup() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    } else {
+      localStorage.setItem("accessToken", token);
+      dispatch(getProfile());
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div className="flex items-center  flex-col">
+      <Navbar />
+      <Frame title="Signup" />
+      <Greeting />
+      <SignupForm />
+    </div>
+  );
+}
+
+export default Signup;
