a3723ae4e2d106e9311172eb9b47eb115fdd5029
Boris Ushakov <inlvert@gmail.com>
2026-01-28 15:43:25 +0200

    add changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,22 @@
+"use client";
+
 import ArticleBox from "@/components/ArticleBox/ArticleBox";
 import BrowseTheRange from "@/components/BrowseTheRange/BrowseTheRange";
+import ButtonMore from "@/components/ButtonMore/ButtonMore";
 import Footer from "@/components/Footer/Footer";
 import Gallery from "@/components/Gallery/Gallery";
 import Navbar from "@/components/Navbar/Navbar";
 import ProductList from "@/components/ProductList/ProductList";
 import Slider from "@/components/Slider/Slider";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push("/shop");
+  };
+
   return (
     <div className="flex items-center  flex-col">
       <Navbar />
@@ -20,6 +30,7 @@ export default function Home() {
       </div>
       <BrowseTheRange />
       <ProductList />
+      <ButtonMore text="Show More" onClick={handleRedirect} />
       <Slider />
       <Gallery />
       <Footer />
