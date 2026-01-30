4559deacf4cb53b5a500dfc7fff4d8a3de20278e
Boris Ushakov <inlvert@gmail.com>
2026-01-30 14:48:13 +0200

    add pagination + styles

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -5,15 +5,29 @@ import ProductItem from "../ProductItem/ProductItem";
 import styles from "./ProductList.module.scss";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { getProducts } from "@/redux/slices/productSlice";
-import ButtonMore from "../ButtonMore/ButtonMore";
 
-export default function ProductList() {
-  const { items, loading, error } = useAppSelector((state) => state.products);
+interface ProductListProps {
+  limit?: number;
+  withPagination?: boolean;
+}
+
+export default function ProductList({
+  limit = 8,
+  withPagination = false,
+}: ProductListProps) {
   const dispatch = useAppDispatch();
 
+  const { items, loading, error, page, totalPages } = useAppSelector(
+    (state) => state.products
+  );
+
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    dispatch(getProducts({ page, limit }));
+  }, [dispatch, page, limit]);
+
+  const handlePageClick = (newPage: number) => {
+    dispatch(getProducts({ page: newPage, limit }));
+  };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
@@ -22,11 +36,40 @@ export default function ProductList() {
     <div className={styles.productList}>
       <section className={styles.container}>
         <h1 className={styles.title}>Our Products</h1>
+
         <div className={styles.productsGrid}>
           {items.map((product: any) => (
             <ProductItem key={product._id} product={product} />
           ))}
         </div>
+
+        {withPagination && totalPages > 1 && (
+          <div className={styles.pagination}>
+            <button
+              disabled={page === 1}
+              onClick={() => handlePageClick(page - 1)}
+            >
+              Prev
+            </button>
+
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((p) => (
+              <button
+                key={p}
+                className={p === page ? styles.activePage : ""}
+                onClick={() => handlePageClick(p)}
+              >
+                {p}
+              </button>
+            ))}
+
+            <button
+              disabled={page === totalPages}
+              onClick={() => handlePageClick(page + 1)}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </section>
     </div>
   );
