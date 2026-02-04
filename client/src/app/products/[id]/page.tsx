a7f4f12099fae2591093cfef9b87c414a4998366
Boris Ushakov <inlvert@gmail.com>
2026-02-04 13:18:37 +0200

    add page product id

---
+++ client/src/app/products/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Navbar from "@/components/Navbar/Navbar";
+import ProductDetailsView from "@/components/ProductDetailsView/ProductDetailsView";
+
+
+async function getProduct(id: string) {
+  const res = await fetch(`http://localhost:5000/products/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch product");
+
+  return res.json();
+}
+
+export default async function ProductDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const product = await getProduct(params.id);
+
+  return (
+    <div>
+      <Navbar />
+      <ProductDetailsView product={product} />;
+    </div>
+  )
+}
