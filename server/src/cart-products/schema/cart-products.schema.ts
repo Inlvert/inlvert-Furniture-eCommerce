2dd2f40dd335f903fa43b5ce31f33c5f33f3c689
Boris Ushakov <inlvert@gmail.com>
2026-02-06 14:13:10 +0200

    add changes to CartProductSchema

--- server/src/cart-products/schema/cart-products.schema.ts
+++ server/src/cart-products/schema/cart-products.schema.ts
@@ -1,27 +1,32 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Types, Document } from "mongoose";
 
 export type CartProductDocument = CartProduct & Document;
 
 @Schema({ timestamps: true })
 export class CartProduct {
-  @Prop({ type: Types.ObjectId, ref: 'Cart', required: true })
-  cartId: Types.ObjectId;
-
-  @Prop({ type: Types.ObjectId, ref: 'Product', required: true })
-  productId: Types.ObjectId;
-
-  @Prop({ type: Number, required: true, default: 1 })
-  quantity: number;
-
-  @Prop({ type: String, required: true, default: 's' })
-  size: string;
-
-  @Prop({ type: String, required: true, default: 'grey' })
-  color: string;
-
-  @Prop({ type: Number, required: true })
-  totalPrice: number;
+  
+  @Prop({ type: Types.ObjectId, ref: "User", required: true, unique: true })
+  userId!: Types.ObjectId;
+  
+  @Prop([
+    {
+      productId: {
+        type: Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1,
+      },
+    },
+  ])
+  items!: {
+    productId: Types.ObjectId;
+    quantity: number;
+  }[];
 }
 
 export const CartProductSchema = SchemaFactory.createForClass(CartProduct);
