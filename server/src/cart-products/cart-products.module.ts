94e7c92721c7432252f8f776980a4ba1c9317851
Boris Ushakov <inlvert@gmail.com>
2026-02-06 14:22:50 +0200

    add changes

--- server/src/cart-products/cart-products.module.ts
+++ server/src/cart-products/cart-products.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { CartProductsController } from './cart-products.controller';
 import { CartProductsService } from './cart-products.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CartProductSchema } from './schema/cart-products.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'CartProduct', schema: require('./schema/cart-products.schema').CartProductSchema },
+      { name: 'CartProduct', schema: CartProductSchema },
     ]),
   ],
   controllers: [CartProductsController],
-  providers: [CartProductsService]
+  providers: [CartProductsService],
+  exports: [MongooseModule],
 })
 export class CartProductsModule {}
