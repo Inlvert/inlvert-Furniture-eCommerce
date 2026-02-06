be1344c2317cca75c90cca2774557d5c0c8a30e3
Boris Ushakov <inlvert@gmail.com>
2026-02-06 14:15:01 +0200

    add changes to userModule

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/users.schema';
+import { CartProductsModule } from 'src/cart-products/cart-products.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    CartProductsModule
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
