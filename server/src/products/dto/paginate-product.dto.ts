908736ad449701549e881ead05a72ab4250965ab
Boris Ushakov <inlvert@gmail.com>
2026-02-23 16:41:15 +0200

    add changes

--- server/src/products/dto/paginate-product.dto.ts
+++ server/src/products/dto/paginate-product.dto.ts
@@ -2,8 +2,8 @@ import { Product } from "../schema/product.schema";
 
 
 export class PaginatedProductsDto {
-  items: Product[];
-  total: number;
-  page: number;
-  totalPages: number;
+  items!: Product[];
+  total!: number;
+  page!: number;
+  totalPages!: number;
 }
