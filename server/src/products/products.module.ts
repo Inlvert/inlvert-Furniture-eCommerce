70cc3ada7731a78be179499c59b37705d4457665
Boris Ushakov <inlvert@gmail.com>
2026-02-23 16:41:59 +0200

    add import Review to ProductsModule

--- server/src/products/products.module.ts
+++ server/src/products/products.module.ts
@@ -3,10 +3,12 @@ import { ProductsService } from './products.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductSchema } from './schema/product.schema';
 import { ProductsController } from './products.controller';
+import { ReviewSchema } from 'src/reviews/schema/reviews.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+    MongooseModule.forFeature([{ name: 'Review', schema: ReviewSchema }]),
   ],
   providers: [ProductsService],
   controllers: [ProductsController],
