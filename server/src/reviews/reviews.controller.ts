bf28c6c9f97cd27804b4b2fe65594b190a7d9f7b
Boris Ushakov <inlvert@gmail.com>
2026-02-25 17:59:54 +0200

    add changes to service

--- server/src/reviews/reviews.controller.ts
+++ server/src/reviews/reviews.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-reviews.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createReviewDto: CreateReviewDto, @Request() req: any) {
+    const userId = req.user?.id;
+
+    console.log('USER ID IN REVIEW CREATION:', userId);
+    if (!userId) {
+      throw new Error('User not authenticated');
+    }
+    return this.reviewsService.create({ ...createReviewDto, userId });
   }
 
   @Get(':id')
