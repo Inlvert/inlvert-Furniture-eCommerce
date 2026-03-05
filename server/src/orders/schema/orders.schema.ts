aecc43b4447e15a04e8da8ea5299c1d6fc3845f3
Boris Ushakov <inlvert@gmail.com>
2026-03-05 15:54:21 +0200

    add order schema

---
+++ server/src/orders/schema/orders.schema.ts
@@ -0,0 +1,72 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types, Document } from 'mongoose';
+
+export type OrderDocument = Order & Document;
+
+@Schema({ timestamps: true })
+export class Order {
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  userId!: Types.ObjectId;
+
+  @Prop([
+    {
+      productId: { type: Types.ObjectId, ref: 'Product', required: true },
+
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1,
+      },
+
+      price: {
+        type: Number,
+        required: true,
+      },
+
+      color: String,
+      size: String,
+    },
+  ])
+  items!: {
+    productId: Types.ObjectId;
+    quantity: number;
+    price: number;
+    color?: string;
+    size?: string;
+  }[];
+
+  @Prop({
+    type: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      country: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      zip: { type: String, required: true },
+      phone: { type: String, required: true },
+      email: { type: String, required: true },
+    },
+    required: true,
+  })
+  billingDetails!: {
+    firstName: string;
+    lastName: string;
+    country: string;
+    address: string;
+    city: string;
+    zip: string;
+    phone: string;
+    email: string;
+  };
+
+  @Prop({ required: true })
+  totalPrice!: number;
+
+  @Prop({
+    enum: ['pending', 'paid', 'shipped', 'delivered', 'cancelled'],
+    default: 'pending',
+  })
+  status!: string;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
