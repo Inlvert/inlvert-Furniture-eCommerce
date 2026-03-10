9de517767970a0d93c4ff7b6e39b9d1ff674fd69
Boris Ushakov <inlvert@gmail.com>
2026-03-10 13:14:32 +0200

    add changes to form

--- client/src/components/CheckoutForm/CheckoutForm.tsx
+++ client/src/components/CheckoutForm/CheckoutForm.tsx
@@ -2,35 +2,66 @@
 
 import styles from "./CheckoutForm.module.scss";
 import { Formik, Form, Field } from "formik";
-import { useAppSelector } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { createOrder } from "@/redux/slices/orderSlice";
 
 export default function CheckoutForm() {
   const { items } = useAppSelector((state) => state.cartProduct);
+  const user = useAppSelector((state) => state.auth?.user);
+  console.log("User in checkout form:", user);
+
+  const dispatch = useAppDispatch();
 
   const total = items.reduce(
     (sum, i) => sum + (i.productId?.price || 0) * (i.quantity || 0),
-    0
+    0,
   );
+
+  const initialValues = {
+    firstName: user?.firstName || "",
+    lastName: user?.lastName || "",
+    company: "Test Company",
+    country: "Test Country",
+    address: "Test Address",
+    city: "Test City",
+    province: "Test Province",// Optional field, can be left empty
+    zip: "11111",
+    phone: "1111111111",
+    email: user?.email || "",
+    note: "Test note", // Optional field, can be left empty
+  };
+
+  const handleSubmit = (values: typeof initialValues) => {
+    const orderData = {
+      items: items.map((item) => ({
+        productId: item.productId._id,
+        quantity: item.quantity,
+        price: item.productId.price,
+      })),
+
+      billingDetails: {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        country: values.country,
+        address: values.address,
+        city: values.city,
+        zip: values.zip,
+        phone: values.phone,
+        email: values.email,
+      },
+
+      totalPrice: total,
+    };
+    console.log("Form submitted with values:", values);
+    dispatch(createOrder(orderData));
+  };
 
   return (
     <div className={styles.wrapper}>
       <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          company: "",
-          country: "",
-          address: "",
-          city: "",
-          province: "",
-          zip: "",
-          phone: "",
-          email: "",
-          note: "",
-        }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
+        enableReinitialize={true}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
       >
         {() => (
           <Form className={styles.container}>
@@ -98,10 +129,7 @@ export default function CheckoutForm() {
               </div>
 
               <div className={styles.field}>
-                <Field
-                  name="note"
-                  placeholder="Additional information"
-                />
+                <Field name="note" placeholder="Additional information" />
               </div>
             </div>
 
@@ -118,9 +146,7 @@ export default function CheckoutForm() {
                     <span>
                       {product.name} × {item.quantity}
                     </span>
-                    <span>
-                      ${(product.price * item.quantity).toFixed(2)}
-                    </span>
+                    <span>${(product.price * item.quantity).toFixed(2)}</span>
                   </div>
                 );
               })}
@@ -142,8 +168,8 @@ export default function CheckoutForm() {
                 </label>
 
                 <p>
-                  Make your payment directly into our bank account.
-                  Please use your Order ID as the payment reference.
+                  Make your payment directly into our bank account. Please use
+                  your Order ID as the payment reference.
                 </p>
 
                 <label>
