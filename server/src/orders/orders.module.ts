957630942245755f0c293d08017f9db89c8a0d6f
Boris Ushakov <inlvert@gmail.com>
2026-03-25 11:33:01 +0200

    add changes to module

--- server/src/orders/orders.module.ts
+++ server/src/orders/orders.module.ts
@@ -3,13 +3,17 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { OrderSchema } from './schema/orders.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PaymentsModule } from 'src/payments/payments.module';
+import { ProductSchema } from 'src/products/schema/product.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Order', schema: OrderSchema }]),
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+    PaymentsModule,
   ],
-  providers: [OrdersService],
+  providers: [OrdersService,],
   controllers: [OrdersController],
-  exports: [MongooseModule],
+  exports: [MongooseModule, PaymentsModule, OrdersService],
 })
 export class OrdersModule {}
