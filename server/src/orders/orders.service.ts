29d83e7660b34a7d4a5c4ea5c6cdcdc4969dd74b
Boris Ushakov <inlvert@gmail.com>
2026-03-25 11:29:29 +0200

    add changes to create order logic + markAsPaid method

--- server/src/orders/orders.service.ts
+++ server/src/orders/orders.service.ts
@@ -2,18 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Order } from './schema/orders.schema';
 import { Model } from 'mongoose';
+import { PaymentsService } from 'src/payments/payments.service';
+import { Product } from 'src/products/schema/product.schema';
 
 @Injectable()
 export class OrdersService {
-  constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
+  constructor(
+    @InjectModel(Order.name) private orderModel: Model<Order>,
+    @InjectModel(Product.name) private productModel: Model<Product>,
+    private paymentsService: PaymentsService,
+  ) {}
 
   async createOrder(
     userId: string,
-    cartId: string,
+    paymentMethod: 'stripe' | 'paypal',
     items: {
       productId: string;
       quantity: number;
-      price: number;
       color?: string;
       size?: string;
     }[],
@@ -26,17 +31,62 @@ export class OrdersService {
       zip: string;
       phone: string;
       email: string;
+      note?: string;
     },
     totalPrice: number,
   ) {
+    const productIds = items.map((i) => i.productId);
+
+    const products = await this.productModel.find({ _id: { $in: productIds } });
+
+    const itemsWithPrice = items.map((item) => {
+      const product = products.find((p) => p._id.toString() === item.productId);
+
+      if (!product) throw new Error('Product not found');
+
+      return {
+        productId: product._id,
+        quantity: item.quantity,
+        price: product.price,
+      };
+    });
+
+    const totalCalculatedPrice = itemsWithPrice.reduce(
+      (sum, i) => sum + i.price * i.quantity,
+      0,
+    );
+
     const newOrder = new this.orderModel({
       userId,
-      cartId,
-      items,
+      items: itemsWithPrice,
       billingDetails,
-      totalPrice,
+      totalPrice: totalCalculatedPrice,
+      status: 'pending',
     });
-    return newOrder.save();
+    newOrder.save();
+
+    const stripeItems = items.map((item) => ({
+      name: item.productId,
+      price:
+        itemsWithPrice.find((i) => i.productId.toString() === item.productId)
+          ?.price || 0,
+      quantity: item.quantity,
+    }));
+
+    if (paymentMethod === 'stripe') {
+      const session = await this.paymentsService.createStripePayment(
+        stripeItems,
+        newOrder._id.toString(),
+      );
+      return { checkoutUrl: session.url };
+    } else if (paymentMethod === 'paypal') {
+      console.log('Create PayPal payment with items:', stripeItems);
+      const session = await this.paymentsService.createPaypalPayment(
+        stripeItems,
+        newOrder._id.toString(),
+      );
+      return { checkoutUrl: session.url };
+    }
   }
 
   async getAllOrders(userId: string) {
@@ -45,5 +95,21 @@ export class OrdersService {
       .populate('items.productId')
       .exec();
     return orders;
+  }
+
+  async markAsPaid(orderId: string) {
+    const order = await this.orderModel.findByIdAndUpdate(
+      orderId,
+      {
+        status: 'paid',
+      },
+      { new: true },
+    );
+    
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    return order;
   }
 }
