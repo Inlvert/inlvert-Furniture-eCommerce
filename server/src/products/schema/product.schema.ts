0010b564cc91beae6036f4c84551fdd23ef1bef5
Boris Ushakov <inlvert@gmail.com>
2026-04-17 16:41:54 +0300

    add new filds to schema

--- server/src/products/schema/product.schema.ts
+++ server/src/products/schema/product.schema.ts
@@ -43,6 +43,81 @@ export class Product {
 
   @Prop({ default: 0 })
   reviewsCount!: number;
+
+  @Prop()
+  salesPackage!: string;
+
+  @Prop()
+  modelNumber!: string;
+
+  @Prop()
+  secondaryMaterial!: string;
+
+  @Prop()
+  configuration!: string;
+
+  @Prop()
+  upholsteryMaterial!: string;
+
+  @Prop()
+  manufacturer!: string;
+
+  @Prop()
+  releaseDate!: Date;
+
+  @Prop()
+  fillingMaterial!: string;
+
+  @Prop()
+  finishType!: string;
+
+  @Prop()
+  adjustableHeadrest!: boolean;
+
+  @Prop({ default: 100 })
+  maximumLoadCapacity!: number;
+
+  @Prop()
+  originOfManufacture!: string;
+
+  @Prop()
+  Width!: number;
+
+  @Prop()
+  Depth!: number;
+
+  @Prop()
+  Height!: number;
+
+  @Prop()
+  Weight!: number;
+
+  @Prop()
+  seatHeight!: number;
+
+  @Prop()
+  legHeight!: number;
+
+  @Prop()
+  warrantySummary!: number;
+
+  @Prop({
+    default:
+      'For Warranty Claims or Any Product Related Issues Please Email at operations@trevifurniture.com',
+  })
+  warrantyServiceType!: string;
+
+  @Prop()
+  coveredInWarranty!: string;
+
+  @Prop({
+    default:
+      'The Warranty Does Not Cover Damages Due To Usage Of The Product Beyond Its Intended Use And Wear & Tear In The Natural Course Of Product Usage.',
+  })
+  notCoveredInWarranty!: string;
+
+  @Prop({ default: 12 })
+  domesticWarranty!: number;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
