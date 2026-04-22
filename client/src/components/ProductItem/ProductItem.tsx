2afdae36aac4a6f289e8aa57612fb33146c04093
Boris Ushakov <inlvert@gmail.com>
2026-04-22 15:41:42 +0300

    add error handling changes + styles

--- client/src/components/ProductItem/ProductItem.tsx
+++ client/src/components/ProductItem/ProductItem.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 import ButtonAddToCart from "../ButtonAddToCart/ButtonAddToCart";
 import styles from "./ProductItem.module.scss";
 import placeholderImg from "@/assets/placeholder.png";
-import { useAppDispatch } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { addProductToCart } from "@/redux/slices/cartProductSlise";
 import { addProductToCompare } from "@/redux/slices/compareSlice";
+import classNames from "classnames";
 
 type Product = {
   _id: string;
@@ -17,7 +18,12 @@ type Product = {
 };
 
 export default function ProductItem({ product }: { product: Product }) {
+  const { error, loading, globalError } = useAppSelector((state) => state.compare);
   const dispatch = useAppDispatch();
+  const compareButtonClass = classNames(styles.detailsLink, {
+    [styles.disabledButton]: error[product._id] === "Product already in compare",
+    [styles.disabledButton2]: error[product._id] === "You can only compare up to 4 products",
+  });
 
   const handleAddToCart = () => {
     dispatch(
@@ -28,9 +34,13 @@ export default function ProductItem({ product }: { product: Product }) {
     );
   };
 
-  const hendleAddToCompare = () => {
+  const hendleAddToCompare = async () => {
     dispatch(addProductToCompare(product._id));
   };
+
+  // if (error === "Product already in compare") {
+  //   alert('цеаe');
+  // }
 
   const imageSrc = product.images?.length
     ? `http://localhost:5000/images/${encodeURIComponent(product.images[0])}`
@@ -60,8 +70,8 @@ export default function ProductItem({ product }: { product: Product }) {
           >
             Details
           </Link>
-          <button onClick={hendleAddToCompare} className={styles.detailsLink}>
-            Compare
+          <button onClick={hendleAddToCompare} className={compareButtonClass}>
+            {error[product._id] ? error[product._id] : "Compare"}
           </button>
         </div>
       </div>
