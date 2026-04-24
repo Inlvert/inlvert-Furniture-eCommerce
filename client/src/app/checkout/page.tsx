0b2c73d7047ea42c8aa1395b4876e29376939d97
Boris Ushakov <inlvert@gmail.com>
2026-04-24 17:00:13 +0300

    add changes

--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -1,14 +1,19 @@
-"use client"
+"use client";
 
-import CheckoutForm from "@/components/CheckoutForm/CheckoutForm"
-import Navbar from "@/components/Navbar/Navbar"
+import CheckoutForm from "@/components/CheckoutForm/CheckoutForm";
+import Feature from "@/components/Feature/Feature";
+import Footer from "@/components/Footer/Footer";
+import Frame from "@/components/Frame/Frame";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function CheckoutPage() {
   return (
     <div className="flex items-center  flex-col">
       <Navbar />
+      <Frame title="Checkout" />
       <CheckoutForm />
-      {/* Add your checkout form and logic here */}
+      <Feature />
+      <Footer />
     </div>
-  )
+  );
 }
--- client/src/app/compare/page.tsx
+++ client/src/app/compare/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import CompareList from "@/components/CompareList/CompareList";
 import Frame from "@/components/Frame/Frame";
 import Navbar from "@/components/Navbar/Navbar";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useEffect } from "react";
-import {getAllCompareProducts} from "@/redux/slices/compareSlice";
+import { getAllCompareProducts } from "@/redux/slices/compareSlice";
 import CompareTable from "@/components/CompareTable/CompareTable";
+import Footer from "@/components/Footer/Footer";
+import Feature from "@/components/Feature/Feature";
 
 export default function ComparePage() {
   const dispatch = useAppDispatch();
@@ -18,9 +19,9 @@ export default function ComparePage() {
     <div className="flex items-center  flex-col">
       <Navbar />
       <Frame title="Compare" />
-      {/* <CompareList /> */}
       <CompareTable />
-      {/* Add your compare page content and logic here */}
+      <Feature />
+      <Footer />
     </div>
   );
 }
