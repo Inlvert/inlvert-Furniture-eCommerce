21b941fde4cf724d9de72e2ea3b1099598eee859
Boris Ushakov <inlvert@gmail.com>
2026-04-27 16:57:35 +0300

    add changes to  SendMail component

--- client/src/components/SendMail/SendMail.tsx
+++ client/src/components/SendMail/SendMail.tsx
@@ -3,8 +3,10 @@
 import styles from "./SendMail.module.scss";
 import { Formik, Form, Field } from "formik";
 import Button from "../Button/Button";
+import * as API from "@/api";
 
 export default function SendMail() {
+
   const initialValues = {
     name: "",
     email: "",
@@ -12,13 +14,25 @@ export default function SendMail() {
     message: "",
   };
 
+  const handleSubmit = async (
+    values: typeof initialValues,
+    { resetForm }: { resetForm: () => void },
+  ) => {
+    try {
+      await API.sendMessage(values);
+
+      resetForm();
+    } catch (error: any) {
+      throw new Error(
+        error.response?.data?.message || "Failed to send message",
+      );
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => console.log(values)}
-      >
-        {() => (
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (
           <Form className={styles.container}>
             <div className={styles.field}>
               <label className={styles.title}>Your Name</label>
@@ -34,9 +48,14 @@ export default function SendMail() {
             </div>
             <div className={styles.field}>
               <label className={styles.title}>Your Message</label>
-              <Field as="textarea" type="text" name="message" placeholder="Your Message" />
+              <Field
+                as="textarea"
+                type="text"
+                name="message"
+                placeholder="Your Message"
+              />
             </div>
-            <Button text="Submit" />
+            <Button text={isSubmitting ? "Sending..." : "Send"} />
           </Form>
         )}
       </Formik>
