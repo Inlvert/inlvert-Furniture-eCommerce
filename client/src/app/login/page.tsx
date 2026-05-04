dd3698fda0bdd8bd410d1dc42bdce75f0bd2977b
Boris Ushakov <inlvert@gmail.com>
2026-05-04 10:47:44 +0300

    add new component loginClient + covered with Suspense

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,38 +1,12 @@
 "use client";
 
-import Footer from "@/components/Footer/Footer";
-import Frame from "@/components/Frame/Frame";
-import Greeting from "@/components/Greeting/Greeting";
-import LoginForm from "@/components/LoginForm/LoginForm";
-import Navbar from "@/components/Navbar/Navbar";
-import { useAppDispatch } from "@/redux/hooks";
-import { getProfile } from "@/redux/slices/authSlice";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import LoginClient from "@/components/LoginClient/LoginClient";
+import { Suspense } from "react";
 
-function Login() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (!token) {
-      return;
-    } else {
-      localStorage.setItem("accessToken", token);
-      dispatch(getProfile());
-    }
-  }, [token, dispatch]);
-
+export default function LoginPage() {
   return (
-    <div className="flex items-center  flex-col">
-      <Navbar />
-      <Frame title="Login" />
-      <Greeting />
-      <LoginForm />
-      <Footer />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginClient />
+    </Suspense>
   );
 }
-
-export default Login;
---
+++ client/src/components/LoginClient/LoginClient.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Footer from "@/components/Footer/Footer";
+import Frame from "@/components/Frame/Frame";
+import Greeting from "@/components/Greeting/Greeting";
+import LoginForm from "@/components/LoginForm/LoginForm";
+import Navbar from "@/components/Navbar/Navbar";
+import { useAppDispatch } from "@/redux/hooks";
+import { getProfile } from "@/redux/slices/authSlice";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+function LoginClient() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    } else {
+      localStorage.setItem("accessToken", token);
+      dispatch(getProfile());
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div className="flex items-center  flex-col">
+      <Navbar />
+      <Frame title="Login" />
+      <Greeting />
+      <LoginForm />
+      <Footer />
+    </div>
+  );
+}
+
+export default LoginClient;
