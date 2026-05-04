8a2ff7e173612d6b99219a3664a4eedb18045e59
Boris Ushakov <inlvert@gmail.com>
2026-05-04 11:05:11 +0300

    add new name page

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import SignupClient from "@/components/SignupClient/SignupClient";
 import { Suspense } from "react";
 
-export default function Page() {
+export default function SignupPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <SignupClient />
