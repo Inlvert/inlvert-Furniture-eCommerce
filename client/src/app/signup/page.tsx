247c2f7b2f0eebbf6431b3217e830ca1353be05d
Boris Ushakov <inlvert@gmail.com>
2026-05-04 10:45:25 +0300

    add new component SignupClient + covered with Suspense

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -1,38 +1,10 @@
-"use client";
+import SignupClient from "@/components/SignupClient/SignupClient";
+import { Suspense } from "react";
 
-import Footer from "@/components/Footer/Footer";
-import Frame from "@/components/Frame/Frame";
-import Greeting from "@/components/Greeting/Greeting";
-import Navbar from "@/components/Navbar/Navbar";
-import SignupForm from "@/components/SingupForm/SingupForm";
-import { useAppDispatch } from "@/redux/hooks";
-import { getProfile } from "@/redux/slices/authSlice";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-function Signup() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (!token) {
-      return;
-    } else {
-      localStorage.setItem("accessToken", token);
-      dispatch(getProfile());
-    }
-  }, [token, dispatch]);
-
+export default function Page() {
   return (
-    <div className="flex items-center  flex-col">
-      <Navbar />
-      <Frame title="Signup" />
-      <Greeting />
-      <SignupForm />
-      <Footer />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignupClient />
+    </Suspense>
   );
 }
-
-export default Signup;
---
+++ client/src/components/SignupClient/SignupClient.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Footer from "@/components/Footer/Footer";
+import Frame from "@/components/Frame/Frame";
+import Greeting from "@/components/Greeting/Greeting";
+import Navbar from "@/components/Navbar/Navbar";
+import SignupForm from "@/components/SingupForm/SingupForm";
+import { useAppDispatch } from "@/redux/hooks";
+import { getProfile } from "@/redux/slices/authSlice";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+function SignupClient() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    } else {
+      localStorage.setItem("accessToken", token);
+      dispatch(getProfile());
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div className="flex items-center  flex-col">
+      <Navbar />
+      <Frame title="Signup" />
+      <Greeting />
+      <SignupForm />
+      <Footer />
+    </div>
+  );
+}
+
+export default SignupClient;
