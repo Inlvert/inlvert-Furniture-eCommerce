c8eb7e7240096d6f716512ce70aa28206c9a453b
Boris Ushakov <inlvert@gmail.com>
2026-05-18 16:03:13 +0300

    add new path to shop page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
   const router = useRouter();
 
   const handleRedirect = () => {
-    router.push("/shop");
+    router.push("/products");
   };
 
   return (
-    <div className="flex items-center  flex-col">
+    <div className="flex items-center flex-col">
       <Navbar />
       <div className="relative flex flex-col justify-center">
         <img
