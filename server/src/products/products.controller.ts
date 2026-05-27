3c3db48df8affebeff83b166a0ff24ff819c6b54
Boris Ushakov <inlvert@gmail.com>
2026-05-27 17:10:19 +0300

    add changes UploadedFiles + update function

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Post,
+  Put,
   Query,
   UploadedFiles,
   UseInterceptors,
@@ -11,7 +13,7 @@ import {
 import { ProductsService } from './products.service';
 import { Product, ProductDocument } from './schema/product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { multerConfig } from '../utils/multer';
 import { PaginatedProductsDto } from './dto/paginate-product.dto';
 
@@ -20,14 +22,30 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  @UseInterceptors(FilesInterceptor('images', 5, multerConfig))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'images', maxCount: 5 },
+        { name: 'additionalImages', maxCount: 5 },
+      ],
+      multerConfig,
+    ),
+  )
   create(
     @Body() createProductDto: CreateProductDto,
-    @UploadedFiles() files: Express.Multer.File[],
+    @UploadedFiles()
+    files: {
+      images?: Express.Multer.File[];
+      additionalImages?: Express.Multer.File[];
+    },
   ): Promise<ProductDocument> {
     console.log('Creating product with data:', createProductDto);
     console.log('Uploaded files:', files);
-    return this.productsService.create(createProductDto, files);
+    return this.productsService.create(
+      createProductDto,
+      files.images || [],
+      files.additionalImages || [],
+    );
   }
 
   @Get()
@@ -37,12 +55,43 @@ export class ProductsController {
   ): Promise<PaginatedProductsDto> {
     const pageNum = parseInt(page) || 1;
     const limitNum = parseInt(limit) || 16;
-    console.log('getAll');
     return this.productsService.findAll(pageNum, limitNum);
   }
 
   @Get(':id')
   findOne(@Param('id') _id: string): Promise<Product | null> {
     return this.productsService.findOne(_id);
+  }
+
+  @Put(':id')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'images', maxCount: 5 },
+        { name: 'additionalImages', maxCount: 5 },
+      ],
+      multerConfig,
+    ),
+  )
+  update(
+    @Param('id') _id: string,
+    @Body() updateProductDto: CreateProductDto,
+    @UploadedFiles()
+    files: {
+      images?: Express.Multer.File[];
+      additionalImages?: Express.Multer.File[];
+    },
+  ): Promise<ProductDocument> {
+    return this.productsService.update(
+      _id,
+      updateProductDto,
+      files.images || [],
+      files.additionalImages || [],
+    );
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
   }
 }
