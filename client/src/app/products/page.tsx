aed7fac6c90507ad32fa2371f5eebd659b8498de
Boris Ushakov <inlvert@gmail.com>
2026-06-19 16:35:39 +0300

    add property to ProductList

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -5,13 +5,15 @@ import Footer from "@/components/Footer/Footer";
 import Frame from "@/components/Frame/Frame";
 import Navbar from "@/components/Navbar/Navbar";
 import ProductList from "@/components/ProductList/ProductList";
+import SortSettings from "@/components/SortSettings/SortSettings";
 
 function Shop() {
   return (
     <div className="flex items-center  flex-col">
       <Navbar />
       <Frame title="Shop" />
-      <ProductList limit={4} withPagination />
+      <SortSettings />
+      <ProductList limit={16} withPagination sort="default" />
       <Feature />
       <Footer />
     </div>
