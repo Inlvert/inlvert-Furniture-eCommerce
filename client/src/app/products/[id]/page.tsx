36246d49220e1a0dd79f8037db7885ac80d6572f
Boris Ushakov <inlvert@gmail.com>
2026-06-26 14:59:25 +0300

    add update ProductDetailsPage

--- client/src/app/products/[id]/page.tsx
+++ client/src/app/products/[id]/page.tsx
@@ -1,35 +1,36 @@
-
-import ButtonMore from "@/components/ButtonMore/ButtonMore";
 import Footer from "@/components/Footer/Footer";
 import FrameV2 from "@/components/FrameV2/FrameV2";
 import Navbar from "@/components/Navbar/Navbar";
 import ProductDetailsView from "@/components/ProductDetailsView/ProductDetailsView";
 import ProductListWithOutPaginate from "@/components/ProductListWithOutPaginate/ProductListWithOutPaginate";
-import router from "next/dist/shared/lib/router/router";
 
 async function getProduct(id: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`, {
-    cache: "no-store",
-  });
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`,
+      { cache: "no-store" },
+    );
 
-  if (!res.ok) throw new Error("Failed to fetch product");
+    if (!res.ok) return null;
 
-  return res.json();
+    return await res.json();
+  } catch (error) {
+    console.log("API error:", error);
+    return null;
+  }
 }
 
 export default async function ProductDetailsPage({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }) {
-  const resolvedParams = await params;
-  const product = await getProduct(resolvedParams.id);
+  const product = await getProduct(params.id);
 
-  // console.log(product);
-  
+  if (!product) return <div>Product not found</div>;
 
   return (
-    <div className="flex items-center  flex-col">
+    <div className="flex items-center flex-col">
       <Navbar />
       <FrameV2 title={product.name} />
       <ProductDetailsView product={product} />
