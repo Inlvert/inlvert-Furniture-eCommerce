600f6ed4931bd89580a74893d3c242322feba582
Boris Ushakov <inlvert@gmail.com>
2026-07-01 14:59:47 +0300

    add checkServerHealth logic to pages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,16 +9,58 @@ import Navbar from "@/components/Navbar/Navbar";
 import ProductListWithOutPaginate from "@/components/ProductListWithOutPaginate/ProductListWithOutPaginate";
 import Slider from "@/components/Slider/Slider";
 import { useRouter } from "next/navigation";
+import Loader from "@/components/Loader/Loader";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import * as API from "@/api";
+import { useEffect, useState } from "react";
+import { getProducts } from "@/redux/slices/productSlice";
 
 export default function Home() {
+  const [serverReady, setServerReady] = useState(false);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (serverReady) return;
+
+    const checkServerHealth = async () => {
+      try {
+        const data = await API.checkHealthServer();
+
+        console.log("Server health check:", data);
+
+        if (data.status === "ok") {
+          console.log("Server is ready", data);
+          setServerReady(true);
+          clearInterval(interval);
+        }
+      } catch (error) {
+        console.log("Error checking server health:", error);
+      }
+    };
+
+    checkServerHealth();
+
+    const interval = setInterval(checkServerHealth, 3000);
+
+    return () => clearInterval(interval);
+  }, [serverReady]);
+
+  useEffect(() => {
+    if (serverReady) {
+      dispatch(getProducts({ page: 1, limit: 8 }));
+    }
+  }, [dispatch, serverReady]);
+
   const router = useRouter();
 
   const handleRedirect = () => {
     router.push("/products");
   };
 
+  
   return (
     <div className="flex items-center flex-col">
+      {!serverReady && <Loader text="Starting the Server..." />}
       <Navbar />
       <div className="relative flex flex-col justify-center">
         <img
@@ -30,11 +72,10 @@ export default function Home() {
       </div>
       <BrowseTheRange />
       <ProductListWithOutPaginate />
-      <ButtonMore text="Show More" onClick={handleRedirect}/>
+      <ButtonMore text="Show More" onClick={handleRedirect} />
       <Slider />
       <Gallery />
       <Footer />
     </div>
   );
 }
-
--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -6,10 +6,51 @@ import Frame from "@/components/Frame/Frame";
 import Navbar from "@/components/Navbar/Navbar";
 import ProductList from "@/components/ProductList/ProductList";
 import SortSettings from "@/components/SortSettings/SortSettings";
+import { useAppDispatch } from "@/redux/hooks";
+import { useEffect, useState } from "react";
+import { getProducts } from "@/redux/slices/productSlice";
+import * as API from "@/api";
+import Loader from "@/components/Loader/Loader";
 
 function Shop() {
+  const [serverReady, setServerReady] = useState(false);
+    const dispatch = useAppDispatch();
+  
+    useEffect(() => {
+      if (serverReady) return;
+  
+      const checkServerHealth = async () => {
+        try {
+          const data = await API.checkHealthServer();
+  
+          console.log("Server health check:", data);
+  
+          if (data.status === "ok") {
+            console.log("Server is ready", data);
+            setServerReady(true);
+            clearInterval(interval);
+          }
+        } catch (error) {
+          console.log("Error checking server health:", error);
+        }
+      };
+  
+      checkServerHealth();
+  
+      const interval = setInterval(checkServerHealth, 3000);
+  
+      return () => clearInterval(interval);
+    }, [serverReady]);
+  
+    useEffect(() => {
+      if (serverReady) {
+        dispatch(getProducts({ page: 1, limit: 8 }));
+      }
+    }, [dispatch, serverReady]);
+    
   return (
     <div className="flex items-center  flex-col">
+      {!serverReady && <Loader text="Starting the Server..." />}
       <Navbar />
       <Frame title="Shop" />
       <SortSettings />
